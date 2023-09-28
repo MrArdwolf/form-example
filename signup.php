1004151ad05b59940f43612755ce279bc446e3f9
Elias Öman <elias@folkestorp.se>
2023-09-28 13:31:27 +0200

    aasd

--- signup.php
+++ signup.php
@@ -1,7 +1,15 @@
+<h1>Hej</h1>
+
 <?php
 var_dump($_GET);
 
 $nameFromForm = $_GET["name"]
-$nameFromForm = $_GET["email"]
+$emailFromForm = $_GET["email"]
+
+echo("you have registerd " . $nameFromForm . " with " . $emailFromForm . ". Thank You!");
+
+$data = $nameFromForm . ", " . $emailFromForm ;
+
+file_put_contents("signups.txt", $data, FILE_APEND);
 
 ?>
